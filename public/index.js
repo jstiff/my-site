844f2d4bf91262e70c51e868738f2c734dcc164c
jstiff <jwstiff8@gmail.com>
2020-08-23 18:09:09 -0500

    website prints off my past proj with setTimeout

--- public/index.js
+++ public/index.js
@@ -1,5 +1,291 @@
-console.log("js");
+let PAST_DATA = [
+  " ",
+  "I",
+  " ",
+  "h",
+  "a",
+  "v",
+  "e",
+  " ",
+  "b",
+  "u",
+  "i",
+  "l",
+  "t",
+  " ",
+  "m",
+  "a",
+  "n",
+  "y",
+  " ",
+  "t",
+  "h",
+  "i",
+  "n",
+  "g",
+  "s",
+  " ",
+  "i",
+  "n",
+  " ",
+  "t",
+  "h",
+  "e",
+  " ",
+  "p",
+  "a",
+  "s",
+  "t",
+  ".",
+  ".",
+  ".",
+  "\n",
+  " ",
+  "T",
+  "i",
+  "m",
+  "b",
+  "e",
+  "r",
+  " ",
+  "F",
+  "r",
+  "a",
+  "m",
+  "e",
+  " ",
+  "h",
+  "o",
+  "m",
+  "e",
+  "s",
+  " ",
+  "f",
+  "r",
+  "o",
+  "m",
+  " ",
+  "l",
+  "o",
+  "g",
+  "s",
+  " ",
+  "I",
+  " ",
+  "c",
+  "u",
+  "t",
+  " ",
+  "d",
+  "o",
+  "w",
+  "n",
+  " ",
+  "m",
+  "y",
+  "s",
+  "e",
+  "l",
+  "f",
+  ".",
+  ".",
+  ".",
+  "\n",
+  " ",
+  "J",
+  "a",
+  "p",
+  "a",
+  "n",
+  "e",
+  "s",
+  "e",
+  " ",
+  "w",
+  "o",
+  "o",
+  "d",
+  " ",
+  "f",
+  "i",
+  "r",
+  "e",
+  "d",
+  " ",
+  "C",
+  "e",
+  "r",
+  "a",
+  "m",
+  "i",
+  "c",
+  " ",
+  "k",
+  "i",
+  "l",
+  "n",
+  "s",
+  " ",
+  "f",
+  "o",
+  "r",
+  " ",
+  "m",
+  "a",
+  "k",
+  "i",
+  "n",
+  "g",
+  " ",
+  "p",
+  "o",
+  "t",
+  "t",
+  "e",
+  "r",
+  "y",
+  ".",
+  ".",
+  ".",
+  "\n",
+  " ",
+  "A",
+  "n",
+  "d",
+  " ",
+  "a",
+  " ",
+  "c",
+  "o",
+  "m",
+  "p",
+  "l",
+  "e",
+  "t",
+  "e",
+  " ",
+  "m",
+  "o",
+  "t",
+  "o",
+  "r",
+  "c",
+  "y",
+  "c",
+  "l",
+  "e",
+  " ",
+  "f",
+  "r",
+  "o",
+  "m",
+  " ",
+  "s",
+  "c",
+  "r",
+  "a",
+  "t",
+  "c",
+  "h",
+  ".",
+  ".",
+  ".",
+];
 
-document.getElementById("past").addEventListener("click", () => {
+let presentPast = () => {
+  for (let i = 0; i < PAST_DATA.length; i++) {
+    (function (i) {
+      setTimeout(function () {
+        document.getElementById("textContainer").innerHTML += PAST_DATA[i];
+        console.log(PAST_DATA[i]);
+      }, 0);
+    })(i);
+  }
+};
+
+//   let typeWriter = (text, i, fnCallback) => {
+//     if (i < text.length) {
+//       document.querySelector("p").innerHTML =
+//         text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+//       setTimeout(function () {
+//         typeWriter(text, i + 1, fnCallback);
+//       }, 100);
+//     } else if (typeof fnCallback == "function") {
+//       setTimeout(fnCallback, 700);
+//     }
+//   };
+
+//   let StartTextAnimation = (i) => {
+//     if (typeof dataText[i] == "undefined") {
+//       setTimeout(() => {
+//         StartTextAnimation(0);
+//       }, 20000);
+//     }
+//     // check if dataText[i] exists
+//     if (i < dataText.length) {
+//       // text exists! start typewriter animation
+//       typeWriter(dataText[i], 0, () => {
+//         // after callback (and whole text has been animated), start next text
+//         StartTextAnimation(i + 1);
+//       });
+//     }
+//   };
+
+//   StartTextAnimation(0);
+
+document.getElementById("past").addEventListener("click", presentPast);
+document.getElementById("present").addEventListener("click", () => {
   alert("my past has stuff");
 });
+document.getElementById("future").addEventListener("click", () => {
+  alert("my past has stuff");
+});
+const area = document.getElementById("textContainer");
+/////////////////////////////
+
+//////////////////////////////////
+
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     // array with texts to type in typewriter
+//     let dataText = past;
+
+//     // type one text in the typwriter
+//     // keeps calling itself until the text is finished;
+//     function typeWriter(text, i, fnCallback) {
+//       // chekc if text isn't finished yet
+//       if (i < text.length) {
+//         // add next character to h1
+//         document.querySelector("p").innerHTML =
+//           text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+//         // wait for a while and call this function again for next character
+//         setTimeout(function () {
+//           typeWriter(text, i + 1, fnCallback);
+//         }, 100);
+//       }
+//       // text finished, call callback if there is a callback function
+//       else if (typeof fnCallback == "function") {
+//         // call callback after timeout
+//         setTimeout(fnCallback, 700);
+//       }
+//     }
+//     // start a typewriter animation for a text in the dataText array
+//     function StartTextAnimation(i) {
+//       if (typeof dataText[i] == "undefined") {
+//         setTimeout(function () {
+//           StartTextAnimation(0);
+//         }, 20000);
+//       }
+//       // check if dataText[i] exists
+//       if (i < dataText[i].length) {
+//         // text exists! start typewriter animation
+//         typeWriter(dataText[i], 0, function () {
+//           // after callback (and whole text has been animated), start next text
+//           StartTextAnimation(i + 1);
+//         });
+//       }
+//     }
+//     // start the text animation
+//     StartTextAnimation(0);
+//   });
--- server/server.js
+++ server/server.js
@@ -4,31 +4,114 @@ const fs = require("fs"),
 const url = require("url");
 const util = require("util");
 const path = require("path");
+let htmlFile;
+let cssFile;
+let jsFile;
+let pastTxtArray;
+let loaded = 0;
 
-const pathName = path.join(__dirname, "../index.html");
+const htmlPath = path.join(__dirname, "../public/index.html");
+const cssPath = path.join(__dirname, "../public/index.css");
+const jsPath = path.join(__dirname, "../public/index.js");
+const pastTxt = path.join(__dirname, "../public/past.txt");
 
-const server = http.createServer(function (req, res) {
-  fs.readFile(pathName, (err, html) => {
+if (loaded < 1) {
+  loaded += 1;
+  fs.readFile(pastTxt, "utf-8", (err, data) => {
     if (err) {
-      console.log(err);
-      res.end();
-    } else {
-      let poop = JSON.stringify(html);
-      console.log("poop", html);
-      res.end(poop);
+      console.log("pastTxt file read ERROR", err);
     }
+    pastTxtArray = data.toString().split("");
+    console.log("PASTtxt,,,,", JSON.stringify(pastTxtArray));
+    fs.appendFile(
+      jsPath,
+      `PAST_DATA = ${JSON.stringify(pastTxtArray)};`,
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log("WORKED");
+      }
+    );
   });
-  //   let path = url.parse(req.url, true);
-  //   console.log("path", path.query);
+}
 
-  //   console.log(`yo...${req.method}...${req.url}`);
-  //   res.writeHead(200, { "Content-Type": "text/html" });
-  //   res.write(util.inspect(path.query));
-  //   res.write(poop);
-  //   let dataObj = { name: "jason", age: 555, ocupation: "yes" };
-  //   let data = JSON.stringify(dataObj);
-
-  //   res.end(poop);
+fs.readFile(cssPath, function (err, data) {
+  if (err) {
+    throw err;
+  }
+  cssFile = data;
+  //   let cssToSend = JSON.stringify(cssFile);
+  //   let cssBuffer = Buffer.from(JSON.parse(cssToSend).data);
+  console.log("css", cssFile);
+});
+fs.readFile(htmlPath, (err, data) => {
+  if (err) {
+    console.log(err);
+    res.end();
+  } else {
+    htmlFile = data;
+  }
 });
 
+fs.readFile(jsPath, (err, data) => {
+  if (err) {
+    console.log(err);
+    res.end();
+  } else {
+    jsFile = data;
+  }
+});
+const server = http.createServer(function (req, res) {
+  switch (req.url) {
+    case "/index.css":
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.write(cssFile);
+      break;
+    case "/index.js":
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+      res.write(jsFile);
+      break;
+    default:
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(htmlFile);
+  }
+  res.end();
+});
+//   let path = url.parse(req.url, true);
+//   console.log("path", path.query);
+
+//   console.log(`yo...${req.method}...${req.url}`);
+
+//   res.write(poop);
+//   let dataObj = { name: "jason", age: 555, ocupation: "yes" };
+//   let data = JSON.stringify(dataObj);
+
+//   res.end(poop);
+
 server.listen(8000);
+
+// try {
+// 	fs.readFile(pastTxt, "utf-8", (err, data) => {
+// 	  if (err) {
+// 		console.log("pastTxt file read ERROR", err);
+// 	  }
+// 	  pastTxtArray = data.toString().split("");
+// 	  console.log("PASTtxt,,,,", JSON.stringify(pastTxtArray));
+// 	  fs.appendFile(
+// 		jsPath,
+// 		`let past = ${JSON.stringify(pastTxtArray)};`,
+// 		(err) => {
+// 		  if (err) {
+// 			console.log(err);
+// 		  }
+// 		  console.log("WORKED");
+// 		}
+// 	  );
+// 	});
+
+// // 	  fs.appendFileSync("./public/index.js", pastArray, "utf8");
+// //	  console.log("poop yo", pastArray);
+//   } catch (err) {
+// 	console.log("REadFile Error", err);
+//   }
